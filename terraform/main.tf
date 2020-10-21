aead80065f80887d56aeb81bcf5fa5287ad4488f
boosh <boosh@example.com>
2020-10-21 21:43:58 +0100

    Convert to cloud-config script

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,5 +4,7 @@ resource "digitalocean_droplet" "wg" {
   region = var.region
   size   = var.size
   ssh_keys = var.ssh_keys
-  user_data = file("${path.module}/templates/user-data.txt")
+  user_data = templatefile("${path.module}/templates/user-data.txt", {
+    wg_init = base64encode(file("${path.module}/templates/wg-init.sh"))
+  })
 }
